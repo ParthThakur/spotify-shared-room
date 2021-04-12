a41259a7d0344a1775f9baaf57ed5270f5c65b99
Parth Thakur <parththakur@gmail.com>
2021-04-12 20:11:41 +0100

    Create pages

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class HomeCreateRoomPage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return <h1>This is the CreateRoomPage!</h1>;
+    }
+}
