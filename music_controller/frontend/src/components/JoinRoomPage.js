179c121e463540bed6aa780bd2f4db8d184c68cb
Parth Thakur <parththakur@gmail.com>
2021-04-13 14:21:01 +0100

    Create: JoinRoomPage

--- music_controller/frontend/src/components/JoinRoomPage.js
+++ music_controller/frontend/src/components/JoinRoomPage.js
@@ -1,11 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-export default class JoinRoomPage extends Component{
-    constructor(props){
-        super(props);
-    }
+export default class JoinRoomPage extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return <h1>This is the join room page</h1>;
-    }
+    this.state = {
+      roomCode: "",
+      error: null,
+    };
+  }
+
+  render() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component={"h4"}>
+            Join a room:
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <TextField
+            error={this.state.error} // TODO: Mateiral-ui expects a boolean.
+            label="Code"
+            placeholder="Enter a room code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="primary">
+            Enter
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
